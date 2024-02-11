ac38cc8772e32ff7d7791d1618989f591a392102
NTTrung9204 <trung9204@gmail.com>
2024-02-11 22:06:10 +0700

    Underfit :(

--- CNN_Identify_Cats_VS Dogs/CNN_Identify_Cats_VS_Dogs.py
+++ CNN_Identify_Cats_VS Dogs/CNN_Identify_Cats_VS_Dogs.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 import cv2
+from keras import Sequential
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten
 
 new_size = (100, 100)
 
@@ -15,11 +17,65 @@ def read_image(folder_path):
             images_np.append(gray_img)
     return np.array(images_np)
 
+def one_hot_encode(num_classes, input_values):
+    encoded_values = np.zeros((len(input_values), num_classes))
+    for i, value in enumerate(input_values):
+        encoded_values[i, value] = 1
+    return encoded_values
+
 img_dog_train = read_image("training_set/dogs")
 img_cat_train = read_image("training_set/cats")
 
-img_train = np.concatenate((img_dog_train, img_cat_train), axis=0)
+img_dog_test = read_image("test_set/dogs")
+img_cat_test = read_image("test_set/cats")
 
-np.random.shuffle(img_train)
+img_train_temp = np.concatenate((img_dog_train, img_cat_train), axis=0)
 
+img_label_temp = one_hot_encode(2, np.array([0]*(img_dog_train.shape[0]) + [1]*(img_cat_train.shape[0])))
 
+print(img_label_temp)
+
+random_indices = np.random.permutation(img_train_temp.shape[0])
+
+img_train = img_train_temp[random_indices]
+img_label = img_label_temp[random_indices]
+
+model = Sequential()
+
+model.add(Conv2D(64, (5, 5), activation="sigmoid", input_shape=(100, 100, 1)))
+
+model.add(MaxPool2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (5, 5), activation="sigmoid"))
+
+model.add(MaxPool2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (5, 5), activation="sigmoid"))
+
+model.add(MaxPool2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+
+model.add(Dense(256, activation="sigmoid"))
+
+model.add(Dense(2, activation="softmax"))
+
+model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
+
+print(img_train.shape, img_label.shape)
+
+model.fit(img_train, img_label, batch_size=32, epochs=10)
+
+img_dog_pred = model.predict(img_dog_test)
+img_cat_pred = model.predict(img_cat_test)
+
+accuracy_score = 0
+for result in img_dog_pred:
+    if np.argmax(result) == 0 : accuracy_score+=1
+
+for result in img_cat_pred:
+    if np.argmax(result) == 1 : accuracy_score+=1
+
+accuracy_score /= (img_dog_pred.shape[0] + img_cat_pred.shape[0])
+
+print(f"Accuracy: {100*accuracy_score:.2f}%")
